d40767b1297c4190f45126feee91a165672e4065
Kirill <droubnier@gmail.com>
2020-11-28 02:22:13 +0300

    added case when snake lenth is 1

--- ConsoleSnake/Snake.cpp
+++ ConsoleSnake/Snake.cpp
@@ -3,7 +3,8 @@
 
 Snake::Snake(int screenWidth, int screenHeight) :
 	screenWidth(screenWidth - 1),
-	screenHeight(screenHeight - 1)
+	screenHeight(screenHeight - 1),
+	direction(Direction::RIGHT)
 {
 	AddTail();
 }
@@ -21,16 +22,39 @@ void Snake::AddTail()
 	auto coords = COORD{ 0, 0 };
 	if (!snake.empty())
 	{
+		int xCoord;
+		int yCoord;
+		auto lastElement = snake.back();
 		if (snake.size() == 1)
 		{
-
+			switch (direction)
+			{
+			case Direction::UP:
+				yCoord = lastElement.Y + 1;
+				xCoord = lastElement.X;
+				break;
+			case Direction::DOWN:
+				yCoord = lastElement.Y - 1;
+				xCoord = lastElement.X;
+				break;
+			case Direction::LEFT:
+				xCoord = lastElement.X + 1;
+				yCoord = lastElement.Y;
+				break;
+			case Direction::RIGHT:
+				xCoord = lastElement.X - 1;
+				yCoord = lastElement.Y;
+				break;
+			}
 		}
-		auto lastElement = snake.back();
-		auto prevElement = snake[snake.size() - 2];
-		auto xCoord = lastElement.X + (lastElement.X - prevElement.X);
-		auto yCoord = lastElement.Y + (lastElement.Y - prevElement.Y);
+		else
+		{
+			auto prevElement = snake[snake.size() - 2];
+			xCoord = lastElement.X + (lastElement.X - prevElement.X);
+			yCoord = lastElement.Y + (lastElement.Y - prevElement.Y);
+		}
 		coords.X = xCoord > -1 ? xCoord : screenWidth;
-		coords.Y = yCoord > -1 ? yCoord : screenWidth;
+		coords.Y = yCoord > -1 ? yCoord : screenHeight;
 	}
 	snake.push_back(coords);
 
